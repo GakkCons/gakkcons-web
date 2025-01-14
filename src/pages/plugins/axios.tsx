ef0f08accfb1c1cbfd5042b892b65b7b6fe35370
JandreiX <102495959+JandreiX@users.noreply.github.com>
2025-01-14 22:16:38 +0000

    Updated app URL - To point towards the API it's hosted on

--- src/pages/plugins/axios.tsx
+++ src/pages/plugins/axios.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000/api/";
+axios.defaults.baseURL = "https://api.gackkons.06222001.xyz/";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 
