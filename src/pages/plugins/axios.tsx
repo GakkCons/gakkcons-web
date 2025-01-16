f0de34cb1df87ed2618c78d7eb99990215c7d6e9
Kent Christian E. Cagadas <46562432+KC878@users.noreply.github.com>
2025-01-16 14:36:51 +0800

    Update axios.tsx - append "api"

--- src/pages/plugins/axios.tsx
+++ src/pages/plugins/axios.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://api.gackkons.06222001.xyz/";
+axios.defaults.baseURL = "https://api.gackkons.06222001.xyz/api";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 
